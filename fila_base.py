c4f547c7ce4cfff7692913a8bd7ade4356e3c433
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-06-07 14:53:19 -0300

    Adiciona metodos abstratos

--- fila_base.py
+++ fila_base.py
@@ -1,4 +1,7 @@
-class FilaBase:
+import abc
+
+
+class FilaBase(metaclass=abc.ABCMeta):
 
     codigo: int = 0
     fila = []
@@ -10,3 +13,15 @@ class FilaBase:
             self.codigo = 0
         else:
             self.codigo += 1
+
+    @abc.abstractmethod
+    def gera_senha(self):
+        ...
+
+    @abc.abstractmethod
+    def atualiza_fila(self):
+        ...
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa: int):
+        ...
