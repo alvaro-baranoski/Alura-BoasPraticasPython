dff25ce4f08f4c94f8794648484e18437e48abfd
alvaro-baranoski <alvarojosebaranoski01@gmail.com>
2022-06-07 15:24:16 -0300

    Adiciona constants.py

--- fila_base.py
+++ fila_base.py
@@ -1,4 +1,5 @@
 import abc
+from constants import TAMANHO_PADRAO_MAXIMO, TAMANHO_PADRAO_MINIMO
 
 
 class FilaBase(metaclass=abc.ABCMeta):
@@ -9,8 +10,8 @@ class FilaBase(metaclass=abc.ABCMeta):
     senha_atual: str = ""
 
     def atualiza_codigo(self) -> None:
-        if self.codigo >= 100:
-            self.codigo = 0
+        if self.codigo >= TAMANHO_PADRAO_MAXIMO:
+            self.codigo = TAMANHO_PADRAO_MINIMO
         else:
             self.codigo += 1
 
